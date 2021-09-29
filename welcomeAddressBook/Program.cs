2954d90c836355c74bf98c8e9b1cd62787f91983
Amit <amitkr24031995@gmail.com>
2021-09-29 15:56:12 +0530

    Adding Welcome Message

---
+++ welcomeAddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace welcomeAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
